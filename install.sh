69ea9fc94003d14e44912b5589e8010bb50d1276
Youssef El Aissaoui <90868691+YusufLisawi@users.noreply.github.com>
2023-02-17 19:01:06 +0100

    Updating README.md

--- install.sh
+++ install.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
+/bin/rm -rf ~/.autogit.sh > /dev/null 2>&1
+
 # Define the download URL and the installation directory
 DOWNLOAD_URL="https://raw.githubusercontent.com/YusufLisawi/autogit/master/autogit.sh"
-INSTALL_DIR="/usr/local/bin"
 
 # Download the script using curl
-curl -o "${INSTALL_DIR}/autogit" "${DOWNLOAD_URL}"
-chmod +x "${INSTALL_DIR}/autogit"
+curl -fsSL "${DOWNLOAD_URL}" > ~/.autogit.sh
+chmod +x ~/.autogit.sh
 
 # Add the installation directory to the system's PATH
-if [ -z "$(grep "${INSTALL_DIR}" ~/.bashrc)" ]; then
-  echo 'export PATH=$PATH:'"${INSTALL_DIR}" >> ~/.bashrc
+if [ -z "$(grep ~/.bashrc)" ]; then
+	echo "source ~/.autogit.sh" >> ~/.bashrc
 fi
 
-if [ -z "$(grep "${INSTALL_DIR}" ~/.zshrc)" ]; then
-  echo 'export PATH=$PATH:'"${INSTALL_DIR}" >> ~/.zshrc
+if [ -z "$(grep ~/.zshrc)" ]; then
+  	echo "source ~/.autogit.sh" >> ~/.zshrc
 fi
 
 # Print a message indicating success
 echo "The autogit command has been installed and added to the system's path."
+echo "run \e[1;32m autogit -help or -h \033[0m for more info"
