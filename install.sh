5b2c27d357a84d58512f116f92932a1fff9e8d6c
YusufLisawi <yusufisawi@gmail.com>
2023-02-18 16:05:36 +0100

    Updating install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 # Define the download URL and the installation directory
 DOWNLOAD_URL="https://raw.githubusercontent.com/YusufLisawi/autogit/master/autogit.sh"
-INSTALL_DIR="$HOME/autogit"
+INSTALL_DIR="$HOME/.autogit"
 
 # Create the installation directory if it doesn't exist
 mkdir -p "$INSTALL_DIR"
@@ -14,10 +14,11 @@ curl -fsSL "${DOWNLOAD_URL}" > "$INSTALL_DIR/autogit.sh"
 /bin/chmod +x "$INSTALL_DIR/autogit.sh"
 
 # Add the installation directory to the system's PATH
-echo "export PATH=\"\$PATH:\$HOME/autogit\"" >> "$HOME/.bashrc"
-echo "export PATH=\"\$PATH:\$HOME/autogit\"" >> "$HOME/.zshrc"
+echo "export PATH=\"\$HOME/.autogit:\$PATH\"" >> "$HOME/.bashrc"
+echo "export PATH=\"\$HOME/.autogit:\$PATH\"" >> "$HOME/.zshrc"
+echo "source \$HOME/.autogit/autogit.sh"
 
-source ~/autogit/autogit.sh
+source ~/.autogit/autogit.sh
 # Print a message indicating success
 echo "The autogit command has been installed and added to the system's path."
 echo "Please restart your shell or run 'source ~/.bashrc' or 'source ~/.zshrc' to use the 'autogit' command."
